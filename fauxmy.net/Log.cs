2d6a0009fc0c128899491c2a1de5e0bc0462484c
Max Burke <maxburke@gmail.com>
2012-10-09 22:53:59 -0700

    Initial commit.

---
+++ fauxmy.net/Log.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace fxmy.net
+{
+    class Log
+    {
+        static Verbosity gVerbosity;
+        static TextWriter gLogStream = Console.Error;
+
+        public enum Verbosity
+        {
+            CRITICAL,
+            ERROR,
+            WARNING,
+            INFO,
+            ALL
+        }
+
+        public static void SetVerbosity(Verbosity verbosityLevel)
+        {
+            gVerbosity = verbosityLevel;
+        }
+
+        public static void SetLogFile(string logFileName)
+        {
+            gLogStream = new StreamWriter(logFileName);
+        }
+    }
+}
---
+++ fauxmy.net/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace fxmy.net
+{
+    class Fauxmy
+    {
+        const int FXMY_DEFAULT_PORT = 3306;
+        const int FXMY_DEFAULT_LISTEN_BACKLOG = 50;
+
+        static Socket OpenSocket(int port)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress address = IPAddress.Any;
+            IPEndPoint endPoint = new IPEndPoint(address, port);
+            socket.Bind(endPoint);
+            socket.Listen(FXMY_DEFAULT_LISTEN_BACKLOG);
+
+            return socket;
+        }
+
+        static void Main(string[] args)
+        {
+            Log.SetVerbosity(Log.Verbosity.ALL);
+            Socket socket = OpenSocket(FXMY_DEFAULT_PORT);
+
+            for (; ; )
+            {
+                Socket newConnection = socket.Accept();
+
+            }
+        }
+    }
+}
