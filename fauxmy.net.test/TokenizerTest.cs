26ea95db0afdf41f7e54f6c0de51586c3446c952
Max Burke <maxburke@gmail.com>
2012-11-25 17:52:34 +0700

    Remove the query log reader for now and just parse the query that was the most broken.

--- fauxmy.net.test/TokenizerTest.cs
+++ fauxmy.net.test/TokenizerTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Diagnostics;
 using fxmy.net;
 
 namespace fxmy.net.test
@@ -15,12 +16,19 @@ namespace fxmy.net.test
             using (TextReader reader = new StreamReader(fxmy.net.Log.QUERY_LOG_FILE))
             {
                 string line = null;
+
+                /*
                 while ((line = reader.ReadLine()) != null)
                 {
                     Query query = new Query(line);
 
                     System.Diagnostics.Debugger.Break();
                 }
+                 */
+
+                const string testQuery = @"CREATE TABLE wp_users (   ID bigint(20) unsigned NOT NULL auto_increment,   user_login varchar(60) NOT NULL default '',   user_pass varchar(64) NOT NULL default '',   user_nicename varchar(50) NOT NULL default '',   user_email varchar(100) NOT NULL default '',   user_url varchar(100) NOT NULL default '',   user_registered datetime NOT NULL default '0000-00-00 00:00:00',   user_activation_key varchar(60) NOT NULL default '',   user_status int(11) NOT NULL default '0',   display_name varchar(250) NOT NULL default '',   PRIMARY KEY  (ID),   KEY user_login_key (user_login),   KEY user_nicename (user_nicename) ) DEFAULT CHARACTER SET utf8";
+                Query query = new Query(testQuery);
+                System.Diagnostics.Debugger.Break();
 
                 return true;
             }
